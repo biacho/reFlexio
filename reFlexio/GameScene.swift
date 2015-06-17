e38ccae4591915c49eaf0118ac0222a3126da2b3
Tobisz Czelakowski <tobiasz@czelakowski.pl>
2015-06-17 02:26:25 +0200

    Implementacja poruszania sie tacki (lewo, prawo), ograniczenie pola w którym tacka reaguje na gest

--- reFlexio/GameScene.swift
+++ reFlexio/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
 	var startBallPosition = CGPoint() // Pocztkowa pozycja piłeczki
 	var startTrayPosition = CGPoint() // Początkowa pozycja tacki
 	var movementDirection = CGPoint() // Kierynek w którym porusza się piłeczka
+	var reflectionAngle = 1
 	var move = Bool(false)
 	
     override func didMoveToView(view: SKView) {
@@ -29,14 +30,19 @@ class GameScene: SKScene {
 		ballSetUp()
 
 		self.addChild(myLabel)
+		
+		let panGesture:UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "moveTray:")
+		view.addGestureRecognizer(panGesture)
     }
 	
+	
+	// BALL
 	func ballSetUp() // Ustawienia piłki
 	{
 		ball.xScale = 0.3
 		ball.yScale = 0.3
 		movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame)) // początkowy kierunek ruchu piłeczki
-		startBallPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+		startBallPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame)) // początkowa pozycja piłeczki
 		ball.position = startBallPosition
 		self.addChild(ball)
 		
@@ -46,23 +52,8 @@ class GameScene: SKScene {
 		
 		}
 	
-	func traySetUp()
-	{
-		tray.size.height = 30
-		tray.size.width = 120
-		startTrayPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame) + tray.size.height)
-		tray.position = startTrayPosition
-		self.addChild(tray)
-	}
-	
-	
-	
 	func letBallMove()
 	{
-		// Log
-		println("letBallMove()")
-		// ---
-		
 		// Wprawić w ruch piłeczkę
 		ball.runAction(SKAction.repeatActionForever(SKAction.sequence([
 			SKAction.runBlock(moveBall),
@@ -73,29 +64,30 @@ class GameScene: SKScene {
 	
 	func moveBall()
 	{
-		// Log
-		//println("\(movementDirection)")
-		//println("\(ball.position)")
-		// ---
-		
 		if (!move) // realod
 		{
-			let moveBall = SKAction.moveTo(movementDirection, duration: 4)
+			let moveBall = SKAction.moveTo(movementDirection, duration: 4.0)
 			ball.runAction(moveBall)
 			move = true
 		}
 		else
 		{
-			if (ball.position.y - ball.size.height/2 <= tray.frame.origin.y + tray.size.height)
+			// TODO: Zaimplementować pozycje tacki przy odbiciu piłeczki
+			if (ball.position.y - ball.size.height/2 <= tray.position.y + tray.size.height) // Tacka
 			{
-				if (ball.position.x <= tray.frame.origin.x || ball.position.x >= tray.frame.size.width)
+				if (ball.position.x >= tray.position.x - tray.size.width/2 || ball.position.x <= tray.position.x + tray.size.width)
 				{
-					println("Zief")
-					println("\(ball.position)")
-					println("\(tray.frame.origin.y - tray.size.height/2), \(tray.size.height), \(tray.frame.size.height)")
-					
-					movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame))
-					move = false
+					switch reflectionAngle
+					{
+					case 0:
+						movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame))
+						move = false
+					case 1:
+						movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame))
+						move = false
+					default:
+						println("Default")
+					}
 				}
 			}
 			else if (ball.position.y - ball.size.height/2 <= CGRectGetMinY(self.frame)) // Dół
@@ -106,7 +98,7 @@ class GameScene: SKScene {
 				move = false
 				ballSetUp()
 			}
-			else if (ball.position.y + ball.size.height/2 >= CGRectGetMaxY(self.frame))
+			else if (ball.position.y + ball.size.height/2 >= CGRectGetMaxY(self.frame)) // Góra
 			{
 				movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame))
 				move = false
@@ -118,7 +110,54 @@ class GameScene: SKScene {
 			
 		}
 	}
+	// ---
 	
+	
+	// TRAY
+	func traySetUp()
+	{
+		tray.size.height = 30
+		tray.size.width = 120
+		startTrayPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame) + tray.size.height)
+		tray.position = startTrayPosition
+		self.addChild(tray)
+	}
+	
+	func moveTray(gesture: UIPanGestureRecognizer)
+	{
+		
+		let velocity = gesture.velocityInView(view!)
+		let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+		let slideMultiplier = magnitude / 20
+		
+		//let speed: CGFloat = abs(20)
+		let speed = gesture.velocityInView(view!).x * 0.05
+		
+		if (gesture.locationInView(view!).y >= self.frame.size.height - tray.size.height * 1.5) // <-- Ograniczenie pola poruszania tacką do dołu ekranu
+		{
+			println("translation: \(gesture.translationInView(view!))")
+			println("location: \(gesture.locationInView(view!))")
+			//println("tray: \(tray.position.x)")
+			println("tray: \(gesture.velocityInView(view!).x)")
+
+			if (gesture.velocityInView(view!).x > 0)
+			{
+				if ((tray.position.x + tray.size.width/2) < self.frame.size.width)
+				{
+					self.tray.runAction(SKAction.moveToX(tray.position.x + slideMultiplier, duration: 0))
+				}
+			}
+			else if (gesture.velocityInView(view!).x < 0)
+			{
+				if ((tray.position.x - tray.size.width/2) > 0)
+				{
+					self.tray.runAction(SKAction.moveToX(tray.position.x - slideMultiplier, duration: 0))
+				}
+			}
+		}
+	}
+	
+	// ---
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
 		letBallMove()
