d21797882e689f2c6f03a36c6283f67e75e1cc50
Tobisz Czelakowski <tobiasz@czelakowski.pl>
2015-07-02 00:27:06 +0200

    Przebudowany mechanizm odbicia piłki.

--- reFlexio/GameScene.swift
+++ reFlexio/GameScene.swift
@@ -12,24 +12,52 @@ class GameScene: SKScene {
 	
 	let ball = SKSpriteNode(imageNamed: "Ball")
 	let tray = SKSpriteNode(imageNamed: "Tray")
+	let gameOverLabel = SKLabelNode(fontNamed:"Chalkduster")
+	
+	var moveBallToDirection = SKAction()
 	var startBallPosition = CGPoint() // Pocztkowa pozycja piłeczki
 	var startTrayPosition = CGPoint() // Początkowa pozycja tacki
 	var movementDirection = CGPoint() // Kierynek w którym porusza się piłeczka
 	var reflectionAngle = 1
-	var move = Bool(false)
+	var move: Bool = true
+	var gameIsOver: Bool = false
+	
+	//var obstacle: Bool = false
+	
+	var x = CGFloat() // do wyznaczania celu poruszania się
+	var y = CGFloat() // do wyznaczania celu poruszania się
+
+	
+	enum Obstacles {
+		case nothing
+		
+		case wallBottom
+		case wallTop
+		case wallLeft
+		case wallRight
+		
+		case tray
+		
+		case brick
+	}
+	
+	var obstaclesType = Obstacles.nothing
+	
+	
 	
     override func didMoveToView(view: SKView) {
 		
+		/* Setup veriables */
+		//var x: CGFloat = CGRectGetMaxX(frame.self)
+		//var y: CGFloat = CGRectGetMaxY(frame.self)
+		
         /* Setup your scene here */
-		let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "reFlexio";
-        myLabel.fontSize = 48;
-        myLabel.position = CGPoint(x: size.width, y: size.height);
+        gameOverLabel.text = "Game Over :(";
+        gameOverLabel.fontSize = 48;
+        gameOverLabel.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2);
 		
 		traySetUp()
 		ballSetUp()
-
-		self.addChild(myLabel)
 		
 		let panGesture:UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "moveTray:")
 		view.addGestureRecognizer(panGesture)
@@ -39,95 +67,212 @@ class GameScene: SKScene {
 	// BALL
 	func ballSetUp() // Ustawienia piłki
 	{
-		ball.xScale = 0.3
-		ball.yScale = 0.3
+		if (UIDevice.currentDevice().userInterfaceIdiom == .Pad) // iPad Mini bez retiny 7.9"
+		{
+			ball.xScale = 0.6
+			ball.yScale = 0.6
+		}
+		else if (UIDevice.currentDevice().userInterfaceIdiom == .Phone) // 0.3 dla iPhone 6 4.7"
+		{
+			ball.xScale = 0.4
+			ball.yScale = 0.4
+		}
+		
 		movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame)) // początkowy kierunek ruchu piłeczki
 		startBallPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame)) // początkowa pozycja piłeczki
+		moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
 		ball.position = startBallPosition
+		ball.childNodeWithName("ball")
 		self.addChild(ball)
 		
 		// Log
-		println("\(ball.position)")
+		print("\(ball.position)")
 		// ---
 		
 		}
 	
 	func letBallMove()
 	{
+		//myLabel.removeFromParent()
+		
 		// Wprawić w ruch piłeczkę
+		moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
+		ball.runAction(moveBallToDirection)
+
 		ball.runAction(SKAction.repeatActionForever(SKAction.sequence([
 			SKAction.runBlock(moveBall),
-			SKAction.waitForDuration(0)
+			SKAction.waitForDuration(0.1)
 			])))
 		// ---
 	}
 	
 	func moveBall()
 	{
-		if (!move) // realod
+		if (ball.position.y /*+ ball.size.height/2*/ <= tray.position.y /*+ tray.size.height*/) // Tray
 		{
-			let moveBall = SKAction.moveTo(movementDirection, duration: 4.0)
-			ball.runAction(moveBall)
-			move = true
+			obstaclesType = Obstacles.tray
 		}
-		else
+		else if (ball.position.y - ball.size.height/2 <= CGRectGetMinY(self.frame)) // Dół
 		{
-			if (ball.position.y - ball.size.height/2 <= tray.position.y + tray.size.height) // Tacka
+			obstaclesType = Obstacles.wallBottom
+		}
+		else if (ball.position.y + ball.size.height/2 >= CGRectGetMaxY(self.frame)) // Góra
+		{
+			obstaclesType = Obstacles.wallTop
+		}
+		else if (ball.position.x + ball.size.width/2 >= CGRectGetMaxX(self.frame)) // Prawa ściana
+		{
+			if (ball.position.y > 0 || ball.position.y < CGRectGetMaxY(self.frame))
 			{
-				if (ball.position.x >= tray.position.x - tray.size.width/2 && ball.position.x <= tray.position.x + tray.size.width)
-				{
-					switch reflectionAngle
-					{
-					case 0:
-						movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame))
-						move = false
-					case 1:
-						movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame))
-						move = false
-					default:
-						println("Default")
-					}
-				}
-				else
-				{
-					println("Game Over!")
-					ball.removeAllActions()
-					ball.removeFromParent()
-					tray.removeAllActions()
-					tray.removeFromParent()
-					move = false
-					ballSetUp()
-					traySetUp()
-				}
+				obstaclesType = Obstacles.wallRight
 			}
-			else if (ball.position.y - ball.size.height/2 <= CGRectGetMinY(self.frame)) // Dół
+		}
+		else if (ball.position.x + ball.size.width/2 <= CGRectGetMinX(self.frame)) // Lewa ściana
+		{
+			if (ball.position.y > 0 || ball.position.y < CGRectGetMaxY(self.frame))
 			{
-				println("Game Over!")
-				ball.removeAllActions()
-				ball.removeFromParent()
-				move = false
-				ballSetUp()
+				obstaclesType = Obstacles.wallLeft
 			}
-			else if (ball.position.y + ball.size.height/2 >= CGRectGetMaxY(self.frame)) // Góra
-			{
-				movementDirection = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame))
-				move = false
-			}
-			else
-			{
-				//println("Position: \(ball.position)")
-			}
-			
+		}
+		
+		checkObstacles()
+	}
+	
+	func checkObstacles()
+	{
+		switch obstaclesType {
+		case .wallTop:
+			//print("Top")
+			hitInTop()
+		case .wallBottom:
+			//print("Bottom")
+			hitInBottom()
+		case .wallLeft:
+			//print("Left Wall")
+			hitInLeftWall()
+		case .wallRight:
+			//print("Right Wall")
+			hitInRightWall()
+		case .tray:
+			//print("Tray")
+			hitInTray()
+		case .brick:
+			//print("Brick")
+			hitInBrick()
+		case .nothing:
+			move = true
+			//print("move: \(move.boolValue)")
 		}
 	}
+	
+	func hitInTray()
+	{
+		if (move)
+		{
+			print("Tacka")
+			
+			x = movementDirection.x
+			y = movementDirection.y
+			
+			print("x: \(x), y: \(y)")
+			print("ball: \(ball.position.x), y: \(ball.position.y + ball.size.height/2)")
+			
+			y = CGRectGetMaxY(frame.self)
+			x = frame.size.width/2
+			
+			movementDirection = CGPoint(x: x, y: y)
+			
+			moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
+			ball.runAction(moveBallToDirection)
+			move = false
+		}
+		
+		obstaclesType = Obstacles.nothing
+	}
+	
+	func hitInTop()
+	{
+		if (move)
+		{
+			print("Sufit")
+			
+			x = movementDirection.x
+			y = movementDirection.y
+			
+			y = CGRectGetMinY(frame.self)
+			x = frame.size.width/2
+			
+			movementDirection = CGPoint(x: x, y: y)
+			
+			moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
+			ball.runAction(moveBallToDirection)
+			move = false
+		}
+		obstaclesType = Obstacles.nothing
+	}
+	
+	func hitInLeftWall()
+	{
+		if (move)
+		{
+			print("Lewa ściana")
+			
+			x = movementDirection.x
+			y = movementDirection.y
+			movementDirection = CGPoint(x: -x, y: y)
+			
+			moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
+			ball.runAction(moveBallToDirection)
+			move = false
+		}
+		obstaclesType = Obstacles.nothing
+	}
+	
+	func hitInRightWall()
+	{
+		if (move)
+		{
+			print("Prawa ściana")
+			x = movementDirection.x
+			y = movementDirection.y
+			movementDirection = CGPoint(x: -x, y: y)
+			
+			moveBallToDirection = SKAction.moveTo(movementDirection, duration: 2.0)
+			ball.runAction(moveBallToDirection)
+
+			move = false
+		}
+		obstaclesType = Obstacles.nothing
+	}
+	func hitInBrick()
+	{
+		print("Not yet :(")
+	}
+	
+	func hitInBottom()
+	{
+		gameOver()
+	}
+	
 	// ---
-	
-	
+
+
 	// TRAY
 	func traySetUp()
 	{
-		tray.size.height = 30
-		tray.size.width = 120
+		if (UIDevice.currentDevice().userInterfaceIdiom == .Pad)
+		{
+			print("iPad")
+			tray.size.height = 50
+			tray.size.width = 200
+		}
+		else if (UIDevice.currentDevice().userInterfaceIdiom == .Phone)
+		{
+			print("iPhone")
+			tray.size.height = 30
+			tray.size.width = 100
+		}
+		
 		startTrayPosition = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMinY(self.frame) + tray.size.height)
 		tray.position = startTrayPosition
 		self.addChild(tray)
@@ -165,11 +310,38 @@ class GameScene: SKScene {
 			self.tray.runAction(SKAction.moveTo(tray.position, duration: 0))
 		}
 	}
-	
 	// ---
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+	
+	func gameOver()
+	{
+		print("Game Over!")
+		self.addChild(gameOverLabel)
+		ball.removeAllActions()
+		ball.removeFromParent()
+		tray.removeAllActions()
+		tray.removeFromParent()
+		move = false
+		gameIsOver = true
+		//ballSetUp()
+		//traySetUp()
+	}
+	
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
-		letBallMove()
+		
+		if (gameIsOver)
+		{
+			ballSetUp()
+			traySetUp()
+			gameOverLabel.removeFromParent()
+			gameIsOver = false
+		}
+		else
+		{
+			letBallMove()
+			//moveBall()
+			//move = true
+		}
 	}
    
     override func update(currentTime: CFTimeInterval) {
